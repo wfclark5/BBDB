667ba80c2be0c708f9a363542f055c9e706b7c47
Duke Center for Autism and Brain Development <34378029+Duke-Autism@users.noreply.github.com>
2019-07-18 20:12:27 -0400

    Inspecting the duplicates in the BBDB

--- bbdb_clean.py
+++ bbdb_clean.py
@@ -1,26 +1,34 @@
 import pandas
+import os.path
+import os
 
-bbdb = pandas.read_csv("bbdb_raw.csv")
+path = os.path.dirname(os.path.realpath(__file__))
 
+bbdb = pandas.read_csv(path + r"\bbdb_all.csv")
 
+##These first few lines will get rid of all data that has bot been completed. This typically happens when someone opens a survey but doesn't do anything else
 
-##These first few lines will get rid of all data that has bot been completed. This typically happens when someone opens a survey but doesn't do anything else 
+# notcompletedfilter = bbdb[(bbdb['firstname'].notna())]
 
+notcompletedfilter = bbdb
 
-notcompletedfilter = bbdb[(bbdb['consent_timestamp'] != "[not completed]") & (bbdb['firstname'].notna())]
+##Use the cleaned data to begin to check for duplicates merge the first name and last names together
 
-
-
-##Use the cleaned data to begin to check for duplicates merge the first name and last names together 
-
-notcompletedfilter["fnlmerge"] = notcompletedfilter['firstname'].str.lower().replace("", "") + notcompletedfilter['lastname'].str.lower().replace("", "")
+notcompletedfilter["fnlmerge"] = notcompletedfilter['firstname'].str.lower().replace("", "") + notcompletedfilter['lastname'].str.lower().replace("", "") + notcompletedfilter['dob'].str.lower()
 
 duplicates = notcompletedfilter
 
 duplicates["is_duplicated"] = notcompletedfilter.duplicated(['fnlmerge'], keep = False)
 
+final_df = duplicates[duplicates["is_duplicated"] == False]
 
-###these commented out lines of code will create your duplicate file 
+final_df.to_csv(path + r"\registry_participants.csv", index=False)
+
+duplicated = duplicates[duplicates["is_duplicated"] == True]
+
+duplicated.to_csv(path + r"\duplicate_potential.csv", index=False)
+
+###these commented out lines of code will create your duplicate file
 
 
 # duplicates = duplicates[(duplicates["is_duplicated"] == True)]
@@ -29,7 +37,3 @@ duplicates["is_duplicated"] = notcompletedfilter.duplicated(['fnlmerge'], keep =
 
 
 ###this is the file that we will want to join back the cleaned duplicate data and the ace participant data
-
-clean_non_duplicated = duplicates[(duplicates["is_duplicated"] == False)]
-
-print(clean_non_duplicated)
--- bbdb_duplicate_handling.py
+++
@@ -1,8 +0,0 @@
-import pandas
-
-bbdb_duplicates = pandas.read_csv("bbdb_duplicates.csv")
-
-df = bbdb_duplicates.groupby(bbdb_duplicates['fnlmerge'])
-df = df.max().reset_index()
-
-df.to_csv("test.csv")
